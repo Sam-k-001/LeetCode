e3ac72d4d66b3cb7878b502b2e190a793f44cd84
Sam-k-001 <Hamang95@outlook.com>
2026-09-23 12:47:34 +0530

    Add: LOOP ()

---
+++ Loop/for-in-loop.js
@@ -0,0 +1,26 @@
+// // let student = {
+// //     name : "Hamang",
+// //     age : 20,
+// //     cpgpa : 6.81,
+// //     isPass : true
+
+// // };
+
+// // for(let i in student){
+// //     console.log("Student Profile:",student[i]);
+// // }
+
+// for(let i =0 ; i <=100;i++){
+//     if(i%2 ==0){
+//         console.log("Its even Number:",i);
+//     }
+// }
+let i =0;
+do{
+    if(i%2==0){
+        console.log("Even number: ", i);
+    }
+    i++;
+    
+}while(i<=100);
+i++;
---
+++ Loop/for-of-loop.js
@@ -0,0 +1,7 @@
+let str = "IwillorIcan";
+let size =0;
+for(let i of str){
+    console.log("Let check What we write it ",i);
+    size++;
+}
+console.log("Size of String:",size);
