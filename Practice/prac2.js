b7971f494d6215ad0b3070d31ba773d6351f011a
Sam-k-001 <Hamang95@outlook.com>
2026-09-24 19:15:40 +0530

    Add: Question (ARRAY)

--- Practice/prac2.js
+++ Practice/prac2.js
@@ -6,4 +6,8 @@ let obj = {
 let output = `The cost of ${obj.item} is ${obj.price} lakh rupees`;
 console.log(output);
 
+//We can add number or whatever we wan to show them addition , subtraction, division, multiplication.
+let comm = `YO YO Honey Singh have ${12+13} lakh income daily`
+console.log(comm);
+
 
